339c832a5c951cdb2bc0d8b0f1c69734e935922c
cruelmoney <chrdengso@gmail.com>
2017-08-22 22:32:33 +0200

    test

--- src/customAnimation.js
+++ src/customAnimation.js
@@ -38,11 +38,10 @@ export default class Animate{
     let elementManipulator = null  
     if (options.target){
       const elements = document.querySelectorAll(options.target)
-      console.log(elements)
       elementManipulator = (val, end) => {
-        // elements.forEach(el=>{
-        //   el.innerHTML = Math.floor(val)
-        // })
+        elements.forEach(el=>{
+          el.innerHTML = Math.floor(val)
+        })
       }
     }
 
