586379864bdb282019c6e93b5eb96c85e8c32e31
cruelmoney <chrdengso@gmail.com>
2017-08-22 15:23:21 +0200

    refactor

--- demos/graph/index.js
+++ demos/graph/index.js
@@ -4,7 +4,14 @@ const bars = document.querySelectorAll("path[id^=bar]")
 const arrowLines = document.querySelectorAll("path[id^=arrow]")
 var idx = 0
 
+//Setup start values for lines (hides them)
 setup = function(){
+  bars.forEach(function(bar) {
+    const len = bar.getTotalLength();
+    bar.style.strokeDasharray = len
+    bar.style.strokeDashoffset = -len + 1 // + 1 -> making it visible before animation
+  })
+
   var len = arrowLines[0].getTotalLength();
   arrowLines[0].style.strokeDasharray = len
   arrowLines[0].style.strokeDashoffset = len 
@@ -16,47 +23,45 @@ setup = function(){
   arrowLines[2].style.strokeDashoffset = -len 
 }
 
-animateLine = function(line, resolver, duration=500){
+//  Helper function to animate a line
+animateLine = function(line, duration=500){
   manipulator = function(val, last){
-    const newVal = val + 0
-    line.style.strokeDashoffset = newVal
+    line.style.strokeDashoffset = val
   }
 
   const start = Number(line.style.strokeDashoffset)
 
-  cudeAnimations.animate(
+  return cudeAnimations.animate(
     manipulator,            //  for each frame call manipulator
     start,                  //  from this value
     0,                      //  to this value
     duration                //  over this duration in ms
-  ).then(resolver)
+  )
 }
 
+
 animateArrow = function(){
-  animateLine(arrowLines[2], function(){
-    animateLine(arrowLines[0])
-    animateLine(arrowLines[1])
-  }, 1200)
-  
+  //First animate line
+  animateLine(arrowLines[2], 1200)
+  //Then animate pointy part
+  .then(
+    function(){
+      animateLine(arrowLines[0])
+      animateLine(arrowLines[1])
+    }
+  )
 }
 
 main = function(){
-  
   setup()
-
+  
   bars.forEach(function(bar) {
-    const len = bar.getTotalLength();
-    bar.style.strokeDasharray = len
-    bar.style.strokeDashoffset = -len + 1 // + 1 -> making it visible before animation
-    
+    // Delay each bar to create overlap
     setTimeout(function(){
       animateLine(bar)
     }, 130*idx++)
-    
-    
   }, this);
 
   animateArrow()
-
 }()
 
