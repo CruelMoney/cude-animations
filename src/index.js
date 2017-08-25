c5e945efdbbf362e8febd0802a53b589a07d3015
cruelmoney <chrdengso@gmail.com>
2017-08-25 16:56:46 +0200

    whoops

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ScrollAnimator from './scrollAnimation'
 import Animate from './customAnimation'
 
 if(typeof(window) !== 'undefined'){
-  import './polyfills'
   window.cudeAnimations = {
     ScrollAnimator, 
     Animate
